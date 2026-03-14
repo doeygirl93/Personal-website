0ee8d9e1ddcca2bec2644665b624fd04870551d8
Chika Ugorji <chikaugorji@outlook.com>
2026-03-14 14:58:30 -0700

    first commit

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+export const Navbar = () => {
+    return <header>
+
+        <nav>
+
+            <a>
+                CU<span>.</span>
+            </a>
+        </nav>
+
+
+
+    </header>
+
+
+};
