834f1ba7140b9e7a1325325341f337c7e651719d
Chika Ugorji <chikaugorji@outlook.com>
2026-03-31 13:30:44 -0700

    Finished hero!

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,18 +2,18 @@
 
 export const Hero = () => {
     return (
-        <section className="relative w-full flex items-center overflow-hidden p-30 ">
+        <section className="relative w-full overflow-hidden p-6 md:p-20 lg:p-40 ">
            
             {/* content */}
-            <div className="grid grid-cols-[1fr_2fr] gap-8 relative z-10 mx-auto">
+            <div className="grid grid-cols-[1fr_1.5fr] gap-15 relative z-10 mx-auto items-center justify-center ">
                 {/*Right col (my cute ahh pfp) */}
-                <div className=" justify-center relative animate-fade-in animation-delay-300">
+                <div className=" justify-center relative animate-fade-in animation-delay-300 mt-10 md:mt-0 max-w-sm mx-auto">
                     {/* pfp */}
                     <div className=" p-4">
                             <h1 className="text-7xl font-bold text-white/85">DoeyGirl</h1>
                             </div>
                     <div className="relative glass rounded-3xl p-2">
-                            <img src="/pfp.jpg" alt="DOeyGirl's pfp" className=" w-full aspect-[4/5] object-cover rounded-2xl"/>
+                            <img src="/pfp.jpg" alt="DOeyGirl's pfp" className=" w-full aspect-[4/5] object-cover rounded-2xl duration-500 hover:scale-101"/>
                         <div className="relative max-w-md mx-auto">
                         </div>
                         <p className="text-lg text-white/60 mt-4 mx-1 text-center">
@@ -27,13 +27,17 @@ export const Hero = () => {
                 </div>
                 
                 
-                {/* Left col (basicaly the text section)*/}
-                <div className="relative border-2 border-amber-100 rounded-2xl">
-                    <img src="/me.jpg" 
+                {/* Left col (basicaly my cute pic)*/}
+                <div className="flex justify-center items-center relative border-2 border-purple-900 rounded-2xl h-[500px] w-[50vw] transition-opacity duration-500 hover:scale-101">
+                    <img src="/me4.png" 
                     alt="Hero img" 
-                    className="w-full h-full object-cover opacity-50 blur-[6px] transition-opacity duration-300 hover:opacity-20"/>
+                    className="w-full h-full object-cover opacity-90 blur-[1px]"/>
                         </div>
-                        
+                        <div>
+                        <p className="text-lg text-white/60 mt-4 mx-1 text-center">
+                                Me irl!.
+                            </p>
+                    </div>
                     </div>
 
              
